d6bdc0e3e700c910504e29559fee454753cd8d10
Piotr Gajowniczek <piotrgajow@gmail.com>
2017-05-03 14:48:01 +0200

    Reorder scripts alphabetically

--- config/git/scripts.sh
+++ config/git/scripts.sh
@@ -1,15 +1,42 @@
-# Utility
-
-function killall {
-    ps | grep "$1" | tr  -s ' ' | cut -d' ' -f2 | xargs kill
+# Git
+function git {
+	if [ "$1" == "pushu" ]
+	then
+		command git rev-parse --abbrev-ref HEAD | xargs git push -u origin
+	else
+		command git "$@"
+	fi
 }
 
 # Grails
-
 export PATH="$PATH:$GRAILS_HOME/bin"
 
-# MySQL
+# Groovy
+export PATH="$PATH:$GROOVY_HOME/bin"
 
+# Liquibase
+export PATH="$PATH:$LIQUIBASE_HOME"
+
+# Maven
+export PATH="$PATH:$MAVEN_HOME/bin"
+
+# mongoDB
+function mongostart {
+    "$MONGO_HOME"/bin/mongod.exe --dbpath "$MONGO_HOME"/data/db > "$MONGO_HOME"/run.log &
+}
+function mongoend {
+    killall mongod
+}
+function mongo {
+    "$MONGO_HOME"/bin/mongo
+}
+function mongorestore {
+    read -p 'Database dump directory: ' DB_DIR
+    "$MONGO_HOME"/bin/mongorestore.exe --drop "$DB_DIR"
+    echo "Restored mongoDB database"
+}
+
+# MySQL
 alias mysqlstart="$MYSQL_HOME/bin/mysqld &"
 alias mysqlend="killall mysqld"
 alias mysql="winpty $MYSQL_HOME/bin/mysql"
@@ -34,39 +61,12 @@ function mysqlimport {
     echo 'Import finished.'
 }
 
-# Liquibase
-
-export PATH="$PATH:$LIQUIBASE_HOME"
-
 # Sublime Text
-
 function subl {
     "$SUBLIME_HOME"/sublime_text.exe $@ &
 }
 
-# mongoDB
-
-function mongostart {
-    "$MONGO_HOME"/bin/mongod.exe --dbpath "$MONGO_HOME"/data/db > "$MONGO_HOME"/run.log &
-}
-function mongoend {
-    killall mongod
-}
-function mongo {
-    "$MONGO_HOME"/bin/mongo
-}
-function mongorestore {
-    read -p 'Database dump directory: ' DB_DIR
-    "$MONGO_HOME"/bin/mongorestore.exe --drop "$DB_DIR"
-    echo "Restored mongoDB database"
-}
-
-# Maven
-
-export PATH="$PATH:$MAVEN_HOME/bin"
-
 # Tomcat
-
 function tomcatstart {
 	"$TOMCAT_HOME"/bin/startup.sh
 }
@@ -75,17 +75,7 @@ function tomcatend {
 	"$TOMCAT_HOME"/bin/shutdown.sh
 }
 
-# Groovy
-
-export PATH="$PATH:$GROOVY_HOME/bin"
-
-# Git
-
-function git {
-	if [ "$1" == "pushu" ]
-	then
-		command git rev-parse --abbrev-ref HEAD | xargs git push -u origin
-	else
-		command git "$@"
-	fi
+# Utility
+function killall {
+    ps | grep "$1" | tr  -s ' ' | cut -d' ' -f2 | xargs kill
 }
