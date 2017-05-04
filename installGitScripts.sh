1aacfd2a9e70ba51e6f73cb1110943f507b987e7
Piotr Gajowniczek <piotrgajow@gmail.com>
2017-05-04 23:41:34 +0200

    Refactor installGitScripts script

--- installGitScripts.sh
+++ installGitScripts.sh
@@ -2,17 +2,21 @@
 
 SCRIPTS=$(realpath $(dirname $0))/config/git/scripts.sh
 
-echo '# Path variables' >> ~/.bashrc
-echo '' >> ~/.bashrc
-echo 'export GRAILS_HOME=' >> ~/.bashrc
-echo 'export GROOVY_HOME=' >> ~/.bashrc
-echo 'export LIQUIBASE_HOME=' >> ~/.bashrc
-echo 'export MAVEN_HOME=' >> ~/.bashrc
-echo 'export MONGO_HOME=' >> ~/.bashrc
-echo 'export MYSQL_HOME=' >> ~/.bashrc
-echo 'export NODE_HOME=' >> ~/.bashrc
-echo 'export SUBLIME_HOME=' >> ~/.bashrc
-echo 'export TOMCAT_HOME=' >> ~/.bashrc
-echo '' >> ~/.bashrc
-echo 'SCRIPTS='"$SCRIPTS" >> ~/.bashrc
-echo 'test -f "$SCRIPTS" && . "$SCRIPTS"' >> ~/.bashrc
+function addConfigLine {
+    echo "$1" >> ~/.bashrc
+}
+
+addConfigLine '# Path variables'
+addConfigLine ''
+addConfigLine 'export GRAILS_HOME='
+addConfigLine 'export GROOVY_HOME='
+addConfigLine 'export LIQUIBASE_HOME='
+addConfigLine 'export MAVEN_HOME='
+addConfigLine 'export MONGO_HOME='
+addConfigLine 'export MYSQL_HOME='
+addConfigLine 'export NODE_HOME='
+addConfigLine 'export SUBLIME_HOME='
+addConfigLine 'export TOMCAT_HOME='
+addConfigLine ''
+addConfigLine 'SCRIPTS='"$SCRIPTS"
+addConfigLine 'test -f "$SCRIPTS" && . "$SCRIPTS"'
