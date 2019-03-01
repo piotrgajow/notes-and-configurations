8839e241326b18903dd29e3a2f3b9a9a863ab086
Piotr Gajowniczek <piotrgajow@gmail.com>
2019-03-01 09:33:00 +0100

    Add aliases for git commands

--- installGitSettings.sh
+++ installGitSettings.sh
@@ -9,6 +9,10 @@ git config --global alias.unhide 'update-index --no-assume-unchanged'
 git config --global alias.show-hidden '!git ls-files -v | grep ^[a-z]'
 git config --global alias.unstage 'reset HEAD'
 git config --global alias.pushu '!git rev-parse --abbrev-ref HEAD | xargs git push -u origin'
+git config --global alias.ap '!git add -N . && git add -p'
+git config --global alias.st 'status'
+git config --global alias.ct 'commit'
+git config --global alias.co 'checkout'
 
 git config --global core.excludesfile '~/.gitignore'
 git config --global fetch.prune true
