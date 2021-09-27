d722c788dbffa6e2ab454c2f9a10209c54b826df
Piotr Gajowniczek <piotrgajow@gmail.com>
2021-09-27 23:13:08 +0200

    update cleanup scripts

--- installGitSettings.sh
+++ installGitSettings.sh
@@ -17,7 +17,8 @@ git config --global alias.co 'checkout'
 git config --global alias.fa 'fetch --all'
 git config --global alias.branch-rename 'branch -m'
 git config --global alias.branch-move 'branch -f'
-git config --global alias.branch-cleanup 'git branch --merged | grep -v master | grep -v develop | tr "\n" " " | tr -s " " | xargs git branch -d'
+git config --global alias.branch-cleanup '!git branch --merged | grep -v master | grep -v develop | tr "\n" " " | tr -s " " | xargs git branch -d'
+git config --global alias.branch-cleanup-alt '!git fetch --prune && git branch -v | grep "\[gone\]" | awk "{print $1}" | xargs git branch -D'
 
 git config --global core.excludesfile '~/.gitignore'
 git config --global fetch.prune true
